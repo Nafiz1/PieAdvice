b3ed9d94dc10cbd56490c2be1a4a9c2e2c320eec
Nafiz1 <nafizh007@gmail.com>
2022-07-27 22:06:48 -0400

    finished share button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 
 import "./App.css";
 
@@ -8,6 +10,10 @@ function getRandomInt(max) {
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef(null);
+    }
     state = { advice: "" };
 
     componentDidMount() {
@@ -27,18 +33,39 @@ class App extends React.Component {
             });
     };
 
+    handleShare = async () => {
+        const newFile = await toBlob(this.imageRef.current);
+        const data = {
+            files: [
+                new File([newFile], "image.png", {
+                    type: newFile.type,
+                }),
+            ],
+            title: "Image",
+            text: "image",
+        };
+        try {
+            await navigator.share(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     render() {
-        <meta charset="utf-8" />;
         const { advice } = this.state;
         return (
             <div className="app">
-                <div className="card">
+                <meta charset="utf-8" />;
+                <div className="card" ref={this.imageRef}>
                     <h1 className="heading">{advice}</h1>
                     <button className="button" onClick={this.fetchAdvice}>
                         <span>GIVE ME ADVICE!</span>
                     </button>
-                    <p className="copyright">© 2022 | Nafiz</p>
+                    <p className="copyright">© 2022 Nafiz</p>
                 </div>
+                <button className="share" onClick={this.handleShare}>
+                    <span>Share</span>
+                </button>
             </div>
         );
     }
